aa3f247dc9263575341fe13ccb0223bccb9df088
Alexey Razmanov <razmanov666@gmail.com>
2021-01-12 11:28:29 +0300

    create templates

--- task_18_4/pizza_restaurant/pizzas/views.py
+++ task_18_4/pizza_restaurant/pizzas/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import Pizza
 def index(request):
     """Домашняя страница приложения."""
     return render(request, "pizzas/index.html")
+
+def pizzas(request):
+    """Отображение всех пицц"""
+    
