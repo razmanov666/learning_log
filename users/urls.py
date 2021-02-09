0ad19473c1dec7d5b321323fb77dbc5838a22132
Alexey Razmanov <razmanov666@gmail.com>
2021-02-09 15:25:22 +0300

    feat: login/logout

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
 """Определяет схемы URL для пользователей."""
-
 from django.urls import re_path
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
     # Страница входа
     re_path(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
-    re_path(r'^logout/$', LogoutView.as_view(template_name='users/index.html'), name='logout'),
+    re_path(r'^logout/$', LogoutView.as_view(template_name='learning_logs/index.html'), name='logout'),
+    re_path(r'^register/$', views.register, name='register'),
+    re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
