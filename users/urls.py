d592ea9cbaf55579f5fea010e881f065501c61b1
Alexey Razmanov <razmanov666@gmail.com>
2021-03-03 17:55:48 +0300

    Error: Invalid form

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,9 @@ from . import views
 urlpatterns = [
     # Страница входа
     re_path(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
+    # Страница выхода
     re_path(r'^logout/$', LogoutView.as_view(template_name='learning_logs/index.html'), name='logout'),
+    # Страница регистрации
     re_path(r'^register/$', views.register, name='register'),
     re_path(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico'))
 ]
--- users/views.py
+++ users/views.py
@@ -1,24 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 
 
 def register(request):
     """Регистрирует нового пользователя."""
-    if request.method != "POST":
+    # print('\nls\n')
+    print('\n'+str(request.method))
+    print(str(request.method) != "POST")
+    print('\n')
+    if str(request.method) != "POST":
         # Display blank registration form.
         form = UserCreationForm()
+        print('\n'+str(form.is_valid())+'\n')
     else:
         # Обработка заполненной формы.
+        print('\nelse\n')
         form = UserCreationForm(data=request.POST)
-        if form.is_valid:
+        if form.is_valid():
             new_user = form.save()
             # Выполнение входа и перенаправление на домашнюю страничку.
             authenticated_user = authenticate(username=new_user.username, 
-                                              password=request.POST['password1'])
+                                              password=request.POST['password1'],
+                                              confirm_password=request.POST['password2'])
             login(request, authenticated_user)
             return HttpResponseRedirect(reverse('index'))
             context = {'form': form}
             return render(request, 'users/register.html', context)
+        else:
+            pass 
